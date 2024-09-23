e022099fa60f99488b97c553458694f401f0e6f1
Gaurav <141632295+Gaurav-Mages@users.noreply.github.com>
2024-09-24 01:13:40 +0530

    Added Bucket, rect and line Tool

--- src/private/PaintApp.cpp
+++ src/private/PaintApp.cpp
@@ -25,10 +25,11 @@ PaintApp::PaintApp()
 	}
 
 	m_brushSize = 1;
+	m_linesToDraw.clear();
 	m_selectedColor = eColor::White;
 	m_selectedTool = eToolType::PencilTool;
-	m_clearColor = std::make_unique<Color>(0);
-
+	isDrawing = false;
+	m_clearColor = std::make_unique<Color>(0.12);
 }
 
 PaintApp::~PaintApp()
@@ -49,12 +50,13 @@ bool PaintApp::init()
 	// set opengl version using glfw
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_RESIZABLE, 0);
 
 	// set openGL backwords & forword compatibitity
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	// Create Window
-	m_window = glfwCreateWindow(1000, 800, "Paint-App", nullptr, nullptr);
+	m_window = glfwCreateWindow(1200,1000, "Paint-App", nullptr, nullptr);
 
 	if (!m_window)
 	{
@@ -96,18 +98,38 @@ void PaintApp::getPlayerInput()
 		m_selectedTool = eToolType::EraserTool;
 		std::cout << "Selected Eraser Tool\n";
 	}
+	if (Input::isButtonDown(Key::L))
+	{
+		m_selectedTool = eToolType::LineTool;
+		std::cout << "Selected Line Tool\n";
+	}
+	if (Input::isButtonDown(Key::K))
+	{
+		m_selectedTool = eToolType::RectTool;
+		std::cout << "Selected Rect Tool\n";
+	}	
+	if (Input::isButtonDown(Key::B))
+	{
+		m_selectedTool = eToolType::BucketTool;
+		std::cout << "Selected Bucket Tool\n";
+	}
 
 	if (Input::isButtonDown(Key::ARROW_UP))
 	{
 		m_brushSize += 1.0;
-		m_brushSize = clamp(m_brushSize, 1, 10);
+		m_brushSize = clamp(m_brushSize, 1, 20);
 	}
 
 	if (Input::isButtonDown(Key::ARROW_DOWN))
 	{
 		m_brushSize -= 1.0;
-		m_brushSize = clamp(m_brushSize, 1, 10);
+		m_brushSize = clamp(m_brushSize, 1, 20);
+	}
 
+	if (Input::isButtonPressed(Key::CTRL) && Input::isButtonDown(Key::Z))
+	{
+		std::cout << "Undo Called\n";
+		m_linesToDraw.pop_back();
 	}
 
 	// Change color with number keys
@@ -179,13 +201,54 @@ void PaintApp::getPlayerInput()
 
 	if (Input::isButtonPressed(Key::MOUSE_LEFT))
 	{
+		isDrawing = true;
 		if (m_selectedTool == eToolType::PencilTool || m_selectedTool == eToolType::EraserTool)
 		{
 			m_points.push_back(getMousePos());
 		}
+		else if (m_selectedTool == eToolType::LineTool)
+		{
+			if (m_points.empty())
+			{
+				m_points.push_back(getMousePos());
+				m_points.push_back(getMousePos());
+			}
+			else
+			{
+				m_points.pop_back();
+				m_points.push_back(getMousePos());
+			}
+		}
+		else if (m_selectedTool == eToolType::RectTool)
+		{
+			if (m_points.empty())
+			{
+				auto [x1, y1] = getMousePos();
+				m_points.push_back({ x1, y1 });
+				auto [x2, y2] = getMousePos();
+				m_points.push_back({ x1, y2 });
+				m_points.push_back({ x2, y2 });
+				m_points.push_back({ x2, y1 });
+				m_points.resize(4);
+			}
+			else
+			{
+				const auto& [x1, y1] = m_points[0];
+				auto [x2, y2] = getMousePos();
+				m_points[1] = { x1, y2 };
+				m_points[2] = { x2, y2 };
+				m_points[3] = { x2, y1 };
+			}
+		}
+		else if(m_selectedTool == eToolType::BucketTool || canFill)
+		{
+			canFill = false;
+			floodFill();
+		}
 	}
-	else
+	else if(isDrawing)
 	{
+		isDrawing = false;
 		if (m_selectedTool == eToolType::PencilTool)
 		{
 			auto tempPoints = std::move(m_points);
@@ -209,6 +272,41 @@ void PaintApp::getPlayerInput()
 				});
 			m_points.clear();
 		}
+		else if (m_selectedTool == eToolType::LineTool)
+		{
+			auto tempPoints = std::move(m_points);
+			float tempSize = m_brushSize;
+			eColor tempclr = m_selectedColor;
+
+			m_linesToDraw.push_back([tempPoints, tempSize, tempclr, this]()
+				{
+					lineTool(tempPoints, tempSize, tempclr);
+				});
+			m_points.clear();
+		}
+		else if (m_selectedTool == eToolType::RectTool)
+		{
+			auto tempPoints = std::move(m_points);
+			float tempSize = m_brushSize;
+			eColor tempclr = m_selectedColor;
+
+			m_linesToDraw.push_back([tempPoints, tempSize, tempclr, this]()
+				{
+					rectTool(tempPoints, tempSize, tempclr);
+				});
+			m_points.clear();
+		}
+		else if (m_selectedTool == eToolType::BucketTool)
+		{
+			auto tempPoints = std::move(m_points);
+			eColor tempclr = m_selectedColor;
+
+			m_linesToDraw.push_back([tempPoints, tempclr, this]()
+				{
+					fillTool(tempPoints, tempclr);
+				});
+			m_points.clear();
+		}
 	}
 }
 
@@ -219,11 +317,14 @@ void PaintApp::run()
 	
 	while (!glfwWindowShouldClose(m_window))
 	{
+		canFill = true;
+		getPlayerInput();
+
 		glfwPollEvents();
+		
 		glClearColor(m_clearColor->r, m_clearColor->g, m_clearColor->b, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		getPlayerInput();
 
 		for (const auto& e : m_linesToDraw)
 		{
@@ -238,31 +339,79 @@ void PaintApp::run()
 		{
 			eraserTool(m_points, m_brushSize);
 		}
+		else if (m_selectedTool == eToolType::LineTool)
+		{
+			lineTool(m_points, m_brushSize, m_selectedColor);
+		}
+		else if (m_selectedTool == eToolType::RectTool)
+		{
+			rectTool(m_points, m_brushSize, m_selectedColor);
+		}
+		else if (m_selectedTool == eToolType::RectTool)
+		{
+			fillTool(m_points, m_selectedColor);
+		}
+
+		drawBorder();
 		
 		glfwSwapBuffers(m_window);
 	}
+}
+
+void PaintApp::drawBorder()
+{
+	glColor3f(0.14, 0.1, 0.25);
+	glBegin(GL_POLYGON);
+
+	glVertex2f(-1.0, 1.0);
+	glVertex2f(-1.0, -1.0);
+	glVertex2f(-0.8, -1.0);
+	glVertex2f(-0.8, 1.0);
+
+	glEnd();
+
+	glBegin(GL_POLYGON);
+
+	glVertex2f(1.0, 1.0);
+	glVertex2f(1.0, -1.0);
+	glVertex2f(0.8, -1.0);
+	glVertex2f(0.8, 1.0);
+
+	glEnd();
 }
 
 
 std::pair<float, float> PaintApp::getMousePos()
 {
 	auto [x, y] = Input::getMousePos();
+	
+	return getMappedPos({x,y});
+}
+
+std::pair<float, float> PaintApp::getMappedPos(std::pair<float, float> pair)
+{
+	auto [x, y] = pair;
 
 	x = (2 * x / m_bufferWidth) - 1;
 	y = 1 - (2 * y / m_bufferHeight);
-	
-	return {x,y};
+
+	return { x, y };
+}
+
+bool PaintApp::isValueSame(const float& a, const float& b, float epsilon)
+{
+	return (fabs(a - b) < epsilon);
 }
 
 void PaintApp::pencilTool(const std::vector<std::pair<float, float>>& points,const float brushSize, eColor clr)
 {
-	Color color = std::move(getColorFromEnum(clr));
+	const Color color = std::move(getColorFromEnum(clr));
 	glColor3f(color.r, color.g, color.b);
 
 	glLineWidth(brushSize);
 	
 	glBegin(GL_LINE_STRIP);
-	for (auto pos : points)
+	for (const auto& pos : points)
 	{
 		glVertex2f(pos.first, pos.second);
 	}
@@ -277,7 +426,7 @@ void PaintApp::eraserTool(const std::vector<std::pair<float, float>>& points, co
 	glLineWidth(brushSize);
 
 	glBegin(GL_LINE_STRIP);
-	for (auto pos : points)
+	for (const auto& pos : points)
 	{
 		glVertex2f(pos.first, pos.second);
 	}
@@ -285,3 +434,100 @@ void PaintApp::eraserTool(const std::vector<std::pair<float, float>>& points, co
 	glEnd();
 }
 
+void PaintApp::lineTool(const std::vector<std::pair<float, float>>& points, const float brushSize, eColor clr)
+{
+	const Color color = std::move(getColorFromEnum(clr));
+	glColor3f(color.r, color.g, color.b);
+
+	glLineWidth(brushSize);
+
+	glBegin(GL_LINE_STRIP);
+
+	for (const auto& pos : points)
+		glVertex2f(pos.first, pos.second);
+
+	glEnd();
+}
+
+void PaintApp::rectTool(const std::vector<std::pair<float, float>>& points, const float brushSize, eColor clr)
+{
+	const Color color = std::move(getColorFromEnum(clr));
+	glColor3f(color.r, color.g, color.b);
+
+	glLineWidth(brushSize);
+
+	glBegin(GL_LINE_LOOP);
+
+	for (const auto& pos : points)
+		glVertex2f(pos.first, pos.second);
+
+	glEnd();
+}
+
+void PaintApp::fillTool(const std::vector<std::pair<float, float>>& points, eColor clr)
+{
+	const Color color = std::move(getColorFromEnum(clr));
+	glColor3f(color.r, color.g, color.b);
+
+	glPointSize(1.0f);
+	glBegin(GL_POINTS);
+
+	for (const auto& pos : points)
+		glVertex2f(pos.first, pos.second);
+
+	glEnd();
+}
+
+int PaintApp::getColorBufferIndex(int x, int y)
+{
+	return ((y * m_bufferWidth) + x) * 3;
+}
+
+void PaintApp::floodFill()
+{
+	const Color targetClr = std::move(getColorFromEnum(m_selectedColor));
+	auto [x, y] = Input::getMousePos();
+
+	if (x < 0 || x >= m_bufferWidth || y < 0 || y >= m_bufferHeight) {
+		return;
+	}
+
+	std::unordered_set<std::pair<int, int>, pair_hash> set;
+
+	std::vector<float> pixelBuffer(m_bufferWidth * m_bufferHeight * 3);
+	glReadPixels(0, 0, m_bufferWidth, m_bufferHeight, GL_RGB, GL_FLOAT, pixelBuffer.data());
+
+	int replacementIndex = getColorBufferIndex(x, m_bufferHeight - y);
+
+	auto queue = std::queue<std::pair<int, int>>();
+	queue.push({ x, y});
+
+	while (!queue.empty())
+	{
+		auto [a, b] = queue.front();
+		queue.pop();
+		if (a <= 0 || a >= m_bufferWidth || b <= 0 || b >= m_bufferHeight )
+			continue;
+
+		if (set.find({a,b}) != set.end())
+			continue;
+
+		float index = getColorBufferIndex(a, m_bufferHeight - b);
+
+		if (isValueSame(pixelBuffer[index+0],pixelBuffer[replacementIndex+0]) &&
+			isValueSame(pixelBuffer[index+1],pixelBuffer[replacementIndex+1]) &&
+			isValueSame(pixelBuffer[index+2],pixelBuffer[replacementIndex+2]))
+		{
+			set.insert({ a,b });
+			queue.push({ a+1,b });
+			queue.push({ a,b+1 });
+			queue.push({ a-1,b });
+			queue.push({ a,b-1 });
+		}
+	}
+
+	for (const auto& e : set)
+	{
+		m_points.push_back(getMappedPos(e));
+	}
+}
--- src/public/PaintApp.h
+++ src/public/PaintApp.h
@@ -4,16 +4,30 @@
 #include <tuple>
 #include <vector>
 #include <functional>
+#include <queue>
+#include <unordered_set>
 
 #include "glew.h"
 #include "glfw3.h"
 #include "Input.h"
 #include "Color.h"
 
+struct pair_hash
+{
+	template <class T1, class T2>
+	std::size_t operator()(const std::pair<T1, T2>& pair) const
+	{
+		return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+	}
+};
+
 enum eToolType
 {
 	PencilTool,
-	EraserTool
+	EraserTool,
+	LineTool,
+	RectTool,
+	BucketTool,
 };
 
 class PaintApp
@@ -27,11 +41,20 @@ public:
 private:
 	bool init();
 	void getPlayerInput();
+	void drawBorder();
 
 	std::pair<float, float> getMousePos();
+	std::pair<float, float> getMappedPos(std::pair<float, float> pair);
+
+	bool isValueSame(const float& a, const float& b, float epsilon = 0.01f);
 
 	void pencilTool(const std::vector<std::pair<float, float>>& points, const float brushSize, eColor clr);
 	void eraserTool(const std::vector<std::pair<float, float>>& points, const float brushSize);
+	void lineTool(const std::vector<std::pair<float, float>>& points, const float brushSize, eColor clr);
+	void rectTool(const std::vector<std::pair<float, float>>& points, const float brushSize, eColor clr);
+	void fillTool(const std::vector<std::pair<float, float>>& points, eColor clr);
+	int getColorBufferIndex(int x, int y);
+	void floodFill();
 
 private:
 	GLFWwindow* m_window;
@@ -40,6 +63,8 @@ private:
 	eToolType m_selectedTool;
 	float m_brushSize;
 	std::unique_ptr<Color> m_clearColor;
+	bool isDrawing;
+	bool canFill;
 	eColor m_selectedColor;
 	std::vector<std::pair<float, float>> m_points;
 	std::vector<std::function<void()>> m_linesToDraw;
